63795f1e266a8d2506d89aa4ae7814c3d96dcb1b
Erik Grundy <erik.grundy@alstefgroup.com>
2024-06-21 09:47:13 +1200

    first commit

---
+++ src/Doot/Program.cs
@@ -0,0 +1,53 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at
+// https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
+};
+
+WeatherForecast GenerateForecast(int index) =>
+    new(
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    );
+
+IEnumerable<WeatherForecast> GenerateForecasts() =>
+    Enumerable.Range(1, 5).Select(GenerateForecast);
+
+app.MapGet("/weatherforecast", GenerateForecasts)
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
